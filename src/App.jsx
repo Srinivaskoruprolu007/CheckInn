f0e622fc840ca4e0dfd95103d26347d68e5bbf08
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-03-27 18:40:00 +0530

    feat (routing) : added basic routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,36 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import GlobalStyles from "./styles/GlobalStyles";
+import Dashboard from "./pages/Dashboard";
+import Account from "./pages/Account";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Login from "./pages/Login";
+import Settings from "./pages/Settings";
+import PageNotFound from "./pages/PageNotFound";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <GlobalStyles />
+      <Router>
+        <Routes>
+          <Route index element={<Navigate replace to="dashboard" />} />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="account" element={<Account />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="login" element={<Login />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </Router>
+    </>
+  );
+};
 
-export default App
+export default App;
